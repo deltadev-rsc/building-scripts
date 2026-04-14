8856a559d54c1fa133baad018a4dbb927bbead38
deltadev-rsc <deltaqxq174@mail.ru>
2026-04-14 13:06:36 +0500

    Update build-op.sh

--- OpenDelta/build-op.sh
+++ OpenDelta/build-op.sh
@@ -6,25 +6,30 @@ function base_actions() {
     mkdir ~/OpenDelts/kernel/obj/
 }
 
-function clone_repo() { git clone https://github.com/deltadev-rsc/OpenDelta.git }
+function clone_repo() { 
+    git clone https://github.com/deltadev-rsc/OpenDelta.git 
+}
 
 #---function-for-build---#
-
 function buildKernel() {
     cd ~/OpenDelta/kernel/
     
     #---compile-asm-code---# 
     nasm boot/i386/boot.asm -f bin -o img/boot.bin    
-    nasm kernel_entry.asm -f elf -o obj/kern_entry.o
-    nasm entry_i386.asm -f elf -o obj/entry_i386.o
-
+    nasm kernel_entry.asm -f elf -o obj/entry.o
+    
     #---compile-kernel---#
     clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c kernel.c -o obj/kernel.o
     clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c cpu/idt.c -o obj/idt.o
     clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c lib/source/stdbase.c -o obj/stdbase.o
-
+    clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c lib/source/string.c -o obj/string.o
+    clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c mem/memory.c -o obj/mem.o
+    clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c ports/ports.c -o obj/ports.o
+    clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c drvs/screen.c -o obj/screen.o
+    clang -m32 -march=i386 -fno-pie -ffreestanding -nostdlib -c lib/source/types.c -o obj/types.o
+        
     #---create-kernel-bin-file---#
-    ld.lld -m elf_i386 -s obj/kernel.o obj/stdbase.o obj/idt.o obj/kern_entry.o obj/entry_i386.o -o img/kernel.bin -z noexecstack -T link.ld -Ttext 0x10000 --oformat binary
+    ld.lld -m elf_i386 -s obj/kernel.o obj/stdbase.o obj/idt.o obj/mem.o obj/string.o obj/types.o obj/screen.o obj/ports.o obj/entry.o -o img/kernel.bin -z noexecstack -T link.ld -Ttext 0x10000 --oformat binary
 
     #---create-os-image---#
     dd if=/dev/zero of=img/open-delta.img bs=512 count=32516 status=none
@@ -37,6 +42,7 @@ function build_shell() {
     while true; do
         clang -c ~/OpenDelta/shell/src/term.c -o ~/OpenDelta/bin/dltsh
         rustc ~/OpenDelta/shell/src/clocks.rs -o ~/OpenDelta/shell/bin/clocks
+        rustc ~/OpenDelta/shell/src/calc.rs -o ~/OpenDelta/shell/bin/calc
         rustc ~/OpenDelta/shell/src/dexide.rs -o ~/OpenDelta/shell/bin/dexide
     done  
 }
