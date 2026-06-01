45bbde527c6a859f27c2c86892a81c3299bd442a
deltadev-rsc <deltaqxq174@mail.ru>
2026-06-01 20:52:52 +0500

    Update buid-phoenix.sh

--- cpp/phoenix-shell/buid-phoenix.sh
+++ cpp/phoenix-shell/buid-phoenix.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-function build() {
+function build {
   cd ~/CPP/terminal/
   mkdir bin/
   g++ terminal.cpp dex.cpp -o bin/term
 }
 
-function run() { ./bin/term }
+function run{ ./bin/term }
 
-build && run
+function main {
+  build && run
+}
+
+main
